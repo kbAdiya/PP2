9ab432d73e96e123620201a1cd81cb8845533a32
kbAdiya <a_khakiyemez.@kbtu.kz>
2024-01-20 15:13:30 +0600

    lastver

--- Hometostring.py
+++ Hometostring.py
@@ -1,8 +1,11 @@
-#comment 
+#comment Python syntax
+print("////// ex1-2")
 print("Hello, World!")
 #Python uses indentation to indicate a block of code
 if 5 > 2:
   print("YES")
+
+print("////// python comments ex 1-2")
 """
 This is a comment
 written in
@@ -13,33 +16,33 @@ A variable name cannot start with a number
 A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
 Variable names are case-sensitive (age, Age and AGE are three different variables)
 A variable name cannot be any of the Python keywords."""
+
+print("//// variavles ex 1-7")
+carname = "Volvo" #str
+print(carname)
 x = 50 # int type
 print(x)
+print("50 type: ", type(x))
+print("volvo type: ", type(carname))
+x=5
 y= 10
 z=x+y
 print(x+y)
 print(z)
-
-carname = "Volvo" #str
-print(carname)
-print("50 type: ", type(x))
-print("volvo type: ", type(carname))
-
-t,w = "John","CARL"
-print(t+w)
-
+x,y,z="orange","banana","cherry"
+print(x,y,z)
+t=w = "John"
+print(t,w)
 print("/////")
 x = "awesome" #global
 
 def myfunc():
   x = "fantastic" #local
   print("Python is " + x)
-
 myfunc()
 
 print("Python is " + x)
-
-
+print("///////")
 
 def myfunc():
   global x   #to make it global to use it outside of function
@@ -50,7 +53,7 @@ myfunc()
 print("Python is " + x)
 
 #data types:
-print("//////")
+print("////// DATA TAPES EX 1-7")
 x=20.5
 print(type(x)) #float
 x = ["apple", "banana", "cherry"]
@@ -63,17 +66,21 @@ x = True
 print(type(x)) #bool
 x = 3+5j
 print(type(x)) #complex
-print("///////")
+
+print("/////// NUMBERS EX 1-3")
 #Convert from one type to another:(You cannot convert complex numbers into another number type.)
+x=5
+x=float(x)
 x=5.5
-print("before: " ,type(x))
+print("5.5 before: " ,type(x))
 x=int(x)
-print("after: ", type(x))
+print("5.5 after: ", type(x))
 x=complex(x)
 print(type(x))
-print("/////")
 
+print("///// STRING EX 1-8")
 x = "Hello World"
+print(x[0])
 print("len of hello world is ", len(x))
 b = "Hello, World!"
 print("characters from position 2 to position 5 (not included) are: ", b[2:5])
